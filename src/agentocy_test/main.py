f0bc65e0ee50cd73db5ac014441e96ebf9ad2344
Manuel Montoya <manuelm1209@hotmail.com>
2024-12-11 17:37:23 -0500

    debigging sqlite3 issue for Streamlit Cloud 1

--- src/agentocy_test/main.py
+++ src/agentocy_test/main.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python
 import sys
-import sys
 import warnings
 import time
 import streamlit as st
 from datetime import datetime
 from crew import AgentocyTest
 
-__import__('pysqlite3')
-sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+# __import__('pysqlite3')
+# sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 # from agentocy_test.crew import AgentocyTest
 
 warnings.filterwarnings("ignore", category=SyntaxWarning, module="pysbd")
